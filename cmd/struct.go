d6aed88fe7e1e7f370ad7fe5aea404c6e799e2ec
unsoop <adamrussak@gmail.com>
2022-08-19 21:46:30 +0000

    added go routins and improved args and command struct

---
+++ cmd/struct.go
@@ -0,0 +1,10 @@
+package cmd
+
+type FlagsOptions struct {
+	Path      string `json:"path,omitempty"`
+	Output    string `json:"output,omitempty"`
+	Overwrite bool   `json:"overwrite,omitempty"`
+	Backup    bool   `json:"backup,omitempty"`
+	DryRun    bool   `json:"dry-run,omitempty"`
+	Version   bool   `json:"version,omitempty"`
+}
