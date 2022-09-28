aa25b3dababc0c3a084ee6421f2575bcc50444bc
Adam Russak <adamrussak@gmail.com>
2022-09-28 14:42:41 +0300

    fix assome role in aws

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
 */
 // TODO: cli commands to be used with DD & Nagios (or other monitoring system)
-
+// URGENT: support ASSOME ROLE
 // TODO: add output flag for: json,csv,toTerminal,pdf
 // PRIORITY: add FIND command to look for a specific Cluster accross Accounts (need to set cloud provder and cluster name)
 // PRIORITY: fix AWS Role output format (just role name is needed to build ARN)
