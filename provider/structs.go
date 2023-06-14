e5968f5ab310a577b7ff42bad36e020a5e007d14
Adam Russak <adamr.ussak@scrdariy.com>
2023-06-14 20:12:03 +0300

    CommandOptions struct

--- provider/structs.go
+++ provider/structs.go
@@ -9,20 +9,21 @@ type AwsProfiles struct {
 }
 
 type CommandOptions struct {
-	AwsRegion     string
-	Path          string
-	Output        string
-	Overwrite     bool
-	Combined      bool
-	Backup        bool
-	Merge         bool
-	ForceMerge    bool
-	UiSize        int
-	DryRun        bool
-	AwsAuth       bool
-	AwsRoleString string
-	AwsEnvProfile bool
-	ProfileName   string
+	AwsRegion       string
+	Path            string
+	Output          string
+	Overwrite       bool
+	Combined        bool
+	Backup          bool
+	Merge           bool
+	ForceMerge      bool
+	UiSize          int
+	DryRun          bool
+	AwsAuth         bool
+	AwsRoleString   string
+	AwsEnvProfile   bool
+	ProfileName     string
+	ProfileSelector bool
 }
 
 // Azure /GCP
